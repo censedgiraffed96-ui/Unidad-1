98b87a510db0004455dfd9dec64d57ebbfe659d1
Arath Cabral <censedgiraffed96@gmail.com>
2025-08-26 17:55:21 -0600

    Add files via upload

---
+++ FDP_EVA1_2_DATOS/EVA1_2_Datos/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author Arath
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // //--->Comentarios de linea
+        //;--->FIN DE INTRODUCCION
+        System.out.println("ARATH CABRAL");
+        System.out.println(1000);//Numero entero
+        System.out.println(3.1416);//Numero decimal
+        System.out.println(true);//Valores logicos Verdadero o Falso
+        System.out.println('A');//Caracter
+        System.out.println("A");//Cadena de Texto
+        
+   
+      }
+    
+}
---
+++ FDP_EVA1_3_PRINT/EVA1_3_PRINT/src/eva1_3_print/EVA1_3_PRINT.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_print;
+
+/**
+ *
+ * @author Arath
+ */
+public class EVA1_3_PRINT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("NOMBRE DEL ESTUDIANTE ");
+        System.out.println("ARATH CABRAL");
+        System.out.print("NUMERO DE CONTROL ");
+        System.out.println("25550715");
+        System.out.print("EDAD ");
+        System.out.println(18);
+    }
+    
+}
