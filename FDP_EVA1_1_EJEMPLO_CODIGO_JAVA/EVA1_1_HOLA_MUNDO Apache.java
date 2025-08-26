f5b73eb8dd4fdbc021f53ab5dc53bd8fe871d2be
Arath Cabral <censedgiraffed96@gmail.com>
2025-08-26 14:47:23 -0600

    Add files via upload

---
+++ FDP_EVA1_1_EJEMPLO_CODIGO_JAVA/EVA1_1_HOLA_MUNDO Apache.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!!");
+        System.out.println("FUNDAMENTOS DE PROGRAMACION");
+    }
+}
