040e47337d6f2426d74bec84fb16a387148cfd8a
Arath Cabral <censedgiraffed96@gmail.com>
2025-08-27 17:51:39 -0600

    Add files via upload

---
+++ FDP_EVA1_5_VARIABLES/EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author Arath
+ */
+public class EVA1_5_VARIABLES {
+
+   
+    public static void main(String[] args) {
+       //DECLARACION DE VARIABLES
+       //CREAR LAS VARIABLES PARA QUE SE PUEDAN USAR
+       //Tipo de dato  identificador
+       String nombre; 
+       byte edad; 
+       //INICIALIZACIOM
+       //USAMOS EL OPERADOR = ---> OPERADOR DE ASIGRNACION
+       //Pone "ARATH CABRAL" DENTRO DE LA VARIABLE DEL NOMBRE
+       nombre = "Arath Cabral";
+       edad = 18;
+       //USO DE VARIABLES
+       System.out.println(nombre);
+       System.out.println(edad);
+    }
+    
+}
