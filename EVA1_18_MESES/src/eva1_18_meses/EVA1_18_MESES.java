8c5657549faded7781437f0729aa3aab96fc6982
Arath Cabral <censedgiraffed96@gmail.com>
2025-09-09 17:47:12 -0600

    Add files via upload

---
+++ EVA1_18_MESES/src/eva1_18_meses/EVA1_18_MESES.java
@@ -0,0 +1,45 @@
+
+package eva1_18_meses;
+
+import java.util.Scanner;
+
+
+public class EVA1_18_MESES {
+
+    
+    public static void main(String[] args) {
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Numero del dia de la semana: ");
+        mes = captu.nextInt();
+        //else - if --> if anidado
+        if (mes == 1){
+          System.out.println("Enero");
+        }else if(mes == 2){
+          System.out.println("Febrero");
+        }else if(mes == 3){
+          System.out.println("Marzo");
+        }else if(mes == 4){
+          System.out.println("Abril");
+        }else if(mes == 5){
+          System.out.println("Mayo");
+        }else if(mes == 6){
+          System.out.println("Junio");
+        }else if(mes == 7){
+          System.out.println("Julio");
+        }else if(mes == 8){
+          System.out.println("Agosto");
+        }else if(mes == 9){
+          System.out.println("Septiembre");
+        }else if(mes == 10){
+          System.out.println("Octubre");
+        }else if(mes == 11){
+          System.out.println("Noviembre");
+        }else if(mes == 12){
+          System.out.println("Diciembre");
+        }else{//Opcional
+            System.out.println("El Mes seleccionado no es valido!! ");
+        }
+    }
+    
+}
